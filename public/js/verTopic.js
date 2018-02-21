e75e2dbcfc1291608c8d12a34d5cc68a5443476f
Nefelijm <sistemasnik20@gmail.com>
2018-02-21 01:08:10 -0500

    Mostrar todo los comentarios y crear comentarios

--- public/js/verTopic.js
+++ public/js/verTopic.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+$(document).ready(function() {
   // Obtener tema individual 
   const url = (`http://examen-laboratoria-sprint-5.herokuapp.com/topics/${localStorage.identifier}`); 
   $.ajax({
@@ -22,8 +22,58 @@ $(document).ready(function() {
     }
   });
   // Mostrar respuestas de un tema
+  const urlRes = `http://examen-laboratoria-sprint-5.herokuapp.com/topics/${localStorage.identifier}/responses`;
+  $.ajax({
+    url: urlRes,
+    contentType: 'application/json',
+    method: 'GET',
+    success: function(response) {
+      console.log(response);   
+    },
 
+    fail: function(request) {
+      if (request) {
+        alert(request.message);
+      }
+    }
+  });
 
   // Crear una nueva respuesta
+  $('#responseTopic').click(function() {
+    const $nameResponse = $('#nameResponse');
+    const $mensage = $('#mensageResponse');
+    let title = $nameResponse.val();
+    let mensages = $mensage.val();
+    let body = {
+      'author_name': title,
+      'content': mensages,
+      'topic_id': localStorage.identifier,
+    };
+  
+    $.ajax({
+      url: (`http://examen-laboratoria-sprint-5.herokuapp.com/topics/${localStorage.identifier}/responses`),
+      // contentType: 'application/json',
+      method: 'POST',
+      data: body,
+      success: function(response) {
+      console.log(response); 
+         $('#topic').append(`
+          <p class="content">ID: ${localStorage.identifier}</p>
+        <p class="name"> AUTOR DEL TEMA: ${title}</p>
+       <p class="content">INFORMACION: ${mensages}</p>
+      
+        </div >`);
+      },
+      fail: function(request) {
+        if (request) {
+          alert(request.message);
+        }
+      }
+    });
+
+
+  });
+
+
 });
 
